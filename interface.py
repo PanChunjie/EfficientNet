7958dc2d38545fd290c77b36d7bd51d2b7965482
Chunjie Pan <pance@mail.uc.edu>
2020-04-08 20:56:07 -0400

    finished all features

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import cv2 as cv
 from PIL import Image, ImageTk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 import time
 import random
 import os
@@ -13,7 +13,7 @@ class Application(tk.Frame):
 
         self.master = master
         self.pack()
-
+        self.photo_index = 0  # the index of photo in snapshot folder
         self.sw = self.winfo_screenwidth()
         self.sh = self.winfo_screenheight()
 
@@ -21,16 +21,32 @@ class Application(tk.Frame):
         self.wh = 600
         self.window_geo()
         self.creat_widgets()
-        self.creat_snapshot_btn()
+
         self.capture = VideoCapture(0)
         #self.master.mainloop()
 
     def window_geo(self):
         self.master.geometry("%dx%d+%d+%d" % (self.wx, self.wh, (self.sw - self.wx) / 2, (self.sh - self.wh) / 2 - 100))
+        #self.master.geometry("800x600")
 
     def creat_widgets(self):
-        self.canvas = tk.Canvas(self, bg='black', height=self.wh*0.9, width=self.wx*0.8)
-        self.canvas.pack(anchor = 'nw')
+        self.tab = ttk.Notebook(self.master)
+        self.tab_home = ttk.Frame(self.tab)
+        self.tab_photo = ttk.Frame(self.tab)
+        self.tab.add(self.tab_home, text = 'Home')
+        self.tab.add(self.tab_photo, text= 'Album')
+        self.tab.pack(expand = 1, fill = 'both')
+        ###### widgets in first tab ######
+        self.canvas = tk.Canvas(self.tab_home, bg='black', height=self.wh*0.9, width=self.wx*0.8)
+        self.canvas.pack(anchor = 'n')
+        self.creat_snapshot_btn()
+        ###### widgets in second tab ######
+        self.label = tk.Label(self.tab_photo, height = int(self.wh*0.9), width=int(self.wx*0.8))
+        self.label.pack()
+        self.creat_album_btn()
+        self.tab.bind("<<NotebookTabChanged>>", lambda event:self.show_photos(event, index=0))
+
+
 
     def show_video(self):
         ret, frame = self.capture.get_frame()
@@ -45,12 +61,55 @@ class Application(tk.Frame):
         self.canvas.after(1, self.show_video)
 
     def creat_snapshot_btn(self):
-        self.snapshot_btn = tk.Button(self, text='shot', width = int(self.wx*0.2/10), height = int(self.wh*0.1/10),
-                                       bg = 'green',command = lambda:self.snapShot()) #
+        self.snapshot_btn = tk.Button(self.tab_home, text='shot', width = int(self.wx*0.2/10), height = int(self.wh*0.1/10),
+                                       bg = 'green',command = lambda:self.snapShot())
         # print(int(self.wx*0.2), int(self.wh*0.1))
         self.snapshot_btn.pack()
         #button3 = tk.Button(self, text="Button3", width=16, height=60)
         #button3.pack()
+
+    def creat_album_btn(self):
+        self.album_frame = tk.Frame(self.tab_photo, height = int(self.wh*0.1))
+        self.album_frame.pack(fill='x', side = 'top')
+
+        self.album_pre_btn = tk.Button(self.album_frame, text='Previous',  width = int(self.wx*0.2/10), height = int(self.wh*0.1/10),
+                                      command = lambda:self.show_photos(index = -1))
+        self.album_next_btn = tk.Button(self.album_frame, text='Next', width=int(self.wx * 0.2 / 10), height=int(self.wh * 0.1 / 10),
+                                      command=lambda: self.show_photos(index = 1))
+        col, row = self.album_frame.grid_size()
+        print('col:',col, 'row', row)
+        self.album_pre_btn.pack(side = 'left', padx = int(self.wh * 0.2))
+        self.album_next_btn.pack(side = 'right', padx = int(self.wh * 0.2))
+        #self.album_pre_btn.grid(row = 0, column = 0,  sticky = 'n', padx = int(self.wh * 0.2))
+        #root.grid_columnconfigure(col, minsize=20)
+        #self.album_next_btn.grid(row = 0, column = 1,  stick = 'n', padx = int(self.wh * 0.2))
+
+    def show_photos(self, event = None, index = 0):
+        index = self.photo_index + index
+        file_path = os.getcwd() + '\\snapshot'
+        if not os.path.isdir(file_path):
+            os.mkdir(file_path)
+        if len(os.listdir(file_path)) == 0:
+            self.label['text'] = 'Album is empty'
+            self.album_pre_btn['state'] = 'disabled'
+            self.album_next_btn['state'] = 'disabled'
+            return
+        else:
+            self.photo_index = index
+            self.album_pre_btn['state'] = 'normal'
+            self.album_next_btn['state'] = 'normal'
+            if index == 0:
+                self.album_pre_btn['state'] = 'disable'
+            if index == len(os.listdir(file_path))-1:
+                self.album_next_btn['state'] = 'disable'
+            imglist = [image for image in os.listdir(file_path)]
+            print('state1: ', self.album_pre_btn['state'])
+            print('state2: ', self.album_pre_btn['state'])
+            print(imglist, ',', index)
+            image = Image.open(os.path.join(file_path, imglist[index]))
+            image = ImageTk.PhotoImage(image)
+            self.label['image'] = image
+            self.label.photo = image
 
     def creat_file_name(self, prefix = 'Driver2.0', suffix = None):
         secondsSinceEpoch = time.time()
@@ -73,48 +132,12 @@ class Application(tk.Frame):
         save_status = cv.imwrite(os.path.join(file_path, file_name), frame)
         if not save_status:
             self.creat_messagebox('Save Photo', 'Fail to save')
-        #self.creat_messagebox('Save Photo', 'Save successfully')
-        myDiag = MyDialog(window=self, title='test', message='test message')
-        myDiag.popup_diag()
+        self.creat_messagebox('Save Photo', 'Save successfully')
+
+
     def creat_messagebox(self, title = None, message = None):
-        self.messagebox = tk.messagebox.showinfo(title,message)
+        self.master.messagebox = tk.messagebox.showinfo(title,message)
 
-class MyDialog:
-    def __init__(self, window, title = 'My Dialog', message = 'Message'):
-        self.window = window
-        self.title = title
-        self.message = message
-
-    def button_func(self, option):
-        if option == 'cancel':
-            print('cancel')
-        else:
-            print('ok')
-
-    def popup_diag(self):
-        tl = tk.Toplevel(master=self.window)
-        tl.title(self.title)
-        frame = tk.Frame(tl)
-        frame.grid()
-
-        canvas = tk.Canvas(frame, width=self.window.wx/3/10, height = self.window.wh/3./10)
-        canvas.grid(row = 1, column = 0)
-        # canvas.grid(row=1, column=0)
-        # imgvar = PhotoImage(file="pyrocket.png")
-        # canvas.create_image(50, 70, image=imgvar)
-        # canvas.image = imgvar
-
-        msgbody1 = tk.Label(frame, text="The", font=("Times New Roman", 10, "bold"))
-        msgbody1.grid(row=1, column=1, sticky='n')
-        # lang = tk.Label(frame, text="language(s)", font=("Times New Roman", 10, "bold"), fg='blue')
-        # lang.grid(row=1, column=2, sticky='n')
-        # msgbody2 = tk.Label(frame, text="of this country is: Arabic", font=("Times New Roman", 20, "bold"))
-        # msgbody2.grid(row=1, column=3, sticky='n')
-        # cancelbttn = tk.Button(frame, text="Cancel", command=lambda: self.button_func("cancel"), width=10)
-        # cancelbttn.grid(row=2, column=3)
-
-        okbttn = tk.Button(frame, text="OK", command=lambda: self.button_func("ok"), width=10)
-        okbttn.grid(row=2, column=4)
 
 class VideoCapture:
     def __init__(self, video_id=0):
